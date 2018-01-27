e5460104584e6786c946e27ceaf6b0e3af5566ce
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-01-27 09:50:43 -0700

    cart controller tested

--- server/controllers/cart_controller.js
+++ server/controllers/cart_controller.js
@@ -2,37 +2,37 @@ const swag = require('../models/swag');
 
 module.exports = {
     add: (req,res,next) => {
-        const {session} = req;
-        const {cart} = session.user
+        //const {session} = req;
+        let {cart} = req.session.user
         const {id} = req.query;
         const index = cart.findIndex(x => x.id == id)
         //we want -1 because that means it was NOT in the cart
         if(index === -1){
             const selectedSwag = swag.find(x => x.id == id);
             cart.push(selectedSwag);
-            session.user.total += selectedSwag.price;
+            req.session.user.total += selectedSwag.price;
         } 
-        res.status(200).send(session.user);
+        res.status(200).send(req.session.user);
     },
 
     delete: (req,res,next) => {
-        const {session} = req;
-        const {cart} = session.user
+        //const {session} = req;
+        let {cart} = req.session.user
         const {id} = req.query;
         const selectedSwag = swag.find(x => x.id == id);
         if(selectedSwag){
             const index = cart.findIndex(x => x.id == id);
             cart.splice(index,1);
-            session.user.total -= selectedSwag.price
+            req.session.user.total -= selectedSwag.price
         }
         
-        res.status(200).send(session.user);
+        res.status(200).send(req.session.user);
     },
 
     checkout: (req,res,next) => {
-        const {session} = req;
-        session.user.cart = [];
-        session.user.total = 0;
-        res.status(200).send(session.user);
+        const {user} = req.session;
+        user.cart = [];
+        user.total = 0;
+        res.status(200).send(req.session.user);
     }
 }
